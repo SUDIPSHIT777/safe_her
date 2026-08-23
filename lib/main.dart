e493083705e198a14cc7521f32bb238fd261c9d7
Sudip Shet <sudipshit777@gmail.com>
2026-08-23 21:34:50 +0530

    all fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:safe_her/features/home/ui/homepage.dart';
 import 'package:safe_her/firebase_options.dart';
 
 void main() async {
@@ -14,6 +15,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
